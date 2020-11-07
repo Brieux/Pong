03827f980544e6c156cf0243f2a3f9e97b451f20
Brieux "Thazerr" Caquelin <33527111+Brieux@users.noreply.github.com>
2020-11-07 15:10:09 +0100

    erreur/oubli dans les nom de var

--- js/pong.js
+++ js/pong.js
@@ -7,6 +7,6 @@ alert(gauche);
 setInterval(function(){
     gauche = gauche + 1;
     haut = haut + 0.5;
-    $("#ball").css("left",gauche);
-    $("#ball").css("top",haut)
+    $("#balle").css("left",gauche);
+    $("#balle").css("top",haut)
 }, 10);
