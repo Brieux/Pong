6736b6a7db6111a008d403cda2b4624affd880ae
Brieux "Thazerr" Caquelin <33527111+Brieux@users.noreply.github.com>
2020-11-11 22:44:38 +0100

    ajout raquette et deplacement

--- js/pong.js
+++ js/pong.js
@@ -63,33 +63,45 @@ class Terrain{
 //implementation classe raquette
 class Raquette{
     //constructeur de la classe Raquette
-    constructor(){
-        this.hauteur = 0;
-        this.largeur = 0;
-        this.positionX = 0;
-        this.positionY = 0;
+    constructor($element){
+        this.$element = $element
+        this.hauteur = $element.height();
+        this.largeur =$element.width();
+        this.positionX = parseInt(this.$element.css("left"));
+        this.positionY = parseInt(this.$element.css("top"));
         this.vitesseY = 1;
     }
     //fonction permettant de faire bouger les deux raquette de haut en bas
     bouger(terrain){
         this.positionY = this.positionY + this.vitesseY;
-        if (this.positionY <= 0 || this.positionY >= terrain.hauteur){
+        if (this.positionY <= 0 || this.positionY + this.hauteur >= terrain.hauteur){
             this.vitesseY = this.vitesseY * -1
         }
+    }
+
+    //fonction de mise a jour graphique de l'objet raquette
+    majHTML(){
+        this.$element.css("left",this.positionX);
+        this.$element.css("top",this.positionY);
     }
 
 }
 //creation des objets
 let terrain = new Terrain($("#terrain"));
 let balle = new Balle($("#balle"));
+let raquetteDroite = new Raquette($("#droite"));
+let raquetteGauche = new Raquette($("#gauche"));
 
 //lancement boucle de jeu
 setInterval(function(){
     balle.bouger();
     balle.rebond(terrain)
     balle.majHTML();
+
+    raquetteDroite.bouger(terrain);
+    raquetteGauche.bouger(terrain);
+    raquetteDroite.majHTML();
+    raquetteGauche.majHTML();
 }, 10);
 
 
-console.log(terrain);
-console.log(balle);
